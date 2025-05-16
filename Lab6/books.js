ddc0d5f872fa47dd2cb063c63da2bc74bdfecf13
Kradinov Anatoly <kradinatoly05@gmail.com>
2025-05-16 15:34:58 +0700

    Книги

---
+++ Lab6/books.js
@@ -0,0 +1,81 @@
+const books = [
+{
+    author: 'Лао-цзы',
+    name: 'Дао дэ цзин. Конон о Дао и дэ',
+    publicationYear: 2022,
+    price: 400,
+    bestseller: false,
+    inStock: true
+},
+{   author: 'Карл Саган',
+    name: 'Косос',
+    publicationYear: 2024,
+    price: 465,
+    bestseller: true,
+    inStock: true
+},
+{
+    author: 'Баладжи Касал',
+    name: 'Ошибки разумного инвестора',
+    publicationYear: 2025,
+    price: 250,
+    bestseller: true,
+    inStock: true
+},
+{
+    author: "Джордж Оруэлл",
+    name: "1984",
+    publicationYear: 1949,
+    price: 450,
+    bestseller: true,
+    inStock: true
+},
+{
+    author: "Рэй Брэдбери",
+    name: "451° по Фаренгейту",
+    publicationYear: 1953,
+    price: 380,
+    bestseller: true,
+    inStock: true
+},
+{
+    author: "Фрэнсис Скотт Фицджеральд",
+    name: "Великий Гэтсби",
+    publicationYear: 1925,
+    price: 520,
+    bestseller: false,
+    inStock: false
+},
+{
+    author: "Михаил Булгаков",
+    name: "Мастер и Маргарита",
+    publicationYear: 1967,
+    price: 490,
+    bestseller: true,
+    inStock: true
+},
+{
+    author: "Эрнест Хемингуэй",
+    name: "Старик и море",
+    publicationYear: 1952,
+    price: 320,
+    bestseller: false,
+    inStock: true
+},
+{
+    author: "Эрих Мария Ремарк",
+    name: "На западном фронте без перемен",
+    publicationYear: 1929,
+    price: 315,
+    bestseller: true,
+    inStock: false
+},
+{
+    author: "Александр Сергеевич Грибоедов",
+    name: "Горе от ума",
+    publicationYear: 1862,
+    price: 400,
+    bestseller: true,
+    inStock: true
+}
+]
